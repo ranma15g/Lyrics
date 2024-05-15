77dc38ac9f6b11fc39eeead112dabce9653408a0
ranma15g <ranma15g@gmail.com>
2024-05-15 19:26:06 +0000

    Updated the code for Unit Test # 2

--- lyrics.cs
+++ lyrics.cs
@@ -7,13 +7,13 @@ using System;
 class Program{
   public static void Main(string[] args)
   {
-    // Unit Test 1 Lyric Line #1
-    Console.WriteLine("Oh, well imagine /");
-    // Unit Test 1 Lyric Line #2
-    Console.WriteLine("As I'm pacing the pews in a church corridor /");
-    // Unit Test 1 Lyric Line #3
-    Console.WriteLine("And I can't help but to hear /");
-    // Unit Test 1 Lyric Line #4
-    Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+    // Unit Test 2 Lyric Line #1
+    Console.WriteLine("Oh-oh-oh-oh-oh,");
+    // Unit Test 2 Lyric Line #2
+    Console.WriteLine("oh-oh-oh-oh,");
+    // Unit Test 2 Lyric Line #3
+    Console.WriteLine("oh-oh-oh /");
+    // Unit Test 2 Lyric Line #4
+    Console.WriteLine("Caught in a bad romance...");
   }
 }
