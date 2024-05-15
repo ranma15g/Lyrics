b7c2be981f16ddf5030074f6391d5f8c56d593bc
ranma15g <ranma15g@gmail.com>
2024-05-15 19:34:59 +0000

    I updated the commit for the Unit 3 testing

--- lyrics.cs
+++ lyrics.cs
@@ -7,13 +7,13 @@ using System;
 class Program{
   public static void Main(string[] args)
   {
-    // Unit Test 2 Lyric Line #1
-    Console.WriteLine("Oh-oh-oh-oh-oh,");
-    // Unit Test 2 Lyric Line #2
-    Console.WriteLine("oh-oh-oh-oh,");
-    // Unit Test 2 Lyric Line #3
-    Console.WriteLine("oh-oh-oh /");
-    // Unit Test 2 Lyric Line #4
-    Console.WriteLine("Caught in a bad romance...");
+    // Unit Test 3 Lyric Line #1
+    Console.WriteLine("Coming out of my cage");
+    // Unit Test 3 Lyric Line #2
+    Console.WriteLine("And I've been doing just fine");
+    // Unit Test 3 Lyric Line #3
+    Console.WriteLine("Gotta gotta be down");
+    // Unit Test 3 Lyric Line #4
+    Console.WriteLine("Because I want it all");
   }
 }
