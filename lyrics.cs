326cc92c971840b59dd83a30554bcf03683a24c5
ranma15g <ranma15g@gmail.com>
2024-05-15 19:15:15 +0000

    My First Commit to this program where I tested the unit 1 batch

---
+++ lyrics.cs
@@ -0,0 +1,19 @@
+using System;
+
+/*
+1-2. Write, compile, and test a program named Lyrics that displays at least four lines of the below Unit Test song lyrics.
+*/
+
+class Program{
+  public static void Main(string[] args)
+  {
+    // Unit Test 1 Lyric Line #1
+    Console.WriteLine("Oh, well imagine /");
+    // Unit Test 1 Lyric Line #2
+    Console.WriteLine("As I'm pacing the pews in a church corridor /");
+    // Unit Test 1 Lyric Line #3
+    Console.WriteLine("And I can't help but to hear /");
+    // Unit Test 1 Lyric Line #4
+    Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+  }
+}
